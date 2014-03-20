f62705e1c2b6f62e302ce11b58295603c9a24629
Miltos Allamanis <m.allamanis@ed.ac.uk>
2014-03-20 16:18:08 +0000

    Print inferred output to html files directly.

--- src/main/java/codemining/java/codeutils/JavaCodePrinter.java
+++ src/main/java/codemining/java/codeutils/JavaCodePrinter.java
@@ -1,4 +1,4 @@
-package codemining.js.codeutils;
+package codemining.java.codeutils;
 
 import java.awt.Color;
 import java.io.File;
@@ -9,10 +9,15 @@ import java.util.SortedMap;
 
 import org.apache.commons.io.FileUtils;
 
-import codemining.java.codeutils.JavaTokenizer;
 import codemining.languagetools.ITokenizer;
 import codemining.languagetools.ITokenizer.FullToken;
 
+/**
+ * Output java code to HTML with optional coloring.
+ * 
+ * @author Miltos Allamanis <m.allamanis@ed.ac.uk>
+ * 
+ */
 public class JavaCodePrinter {
 
 	/**
@@ -47,7 +52,33 @@ public class JavaCodePrinter {
 		}
 	}
 
-	private static void addSlack(final String substring, final StringBuffer buf) {
+	/**
+	 * The tokenizer used to tokenize code.
+	 */
+	final ITokenizer jTokenizer;
+
+	/**
+	 * The background Color of the output HTML document.
+	 */
+	final Color documentBackgroundColor;
+
+	public JavaCodePrinter() {
+		jTokenizer = new JavaTokenizer();
+		documentBackgroundColor = Color.WHITE;
+	}
+
+	public JavaCodePrinter(final Color documentBackgroundColor) {
+		jTokenizer = new JavaTokenizer();
+		this.documentBackgroundColor = documentBackgroundColor;
+	}
+
+	public JavaCodePrinter(final ITokenizer tokenizer,
+			final Color documentBackgroundColor) {
+		this.jTokenizer = tokenizer;
+		this.documentBackgroundColor = documentBackgroundColor;
+	}
+
+	private void addSlack(final String substring, final StringBuffer buf) {
 		for (final char c : substring.toCharArray()) {
 			if (c == ' ') {
 				buf.append("&nbsp;");
@@ -62,27 +93,26 @@ public class JavaCodePrinter {
 	}
 
 	/**
-	 * @param lm
-	 * @param codeFile
-	 * @throws IOException
-	 * @throws IllegalAccessException
-	 * @throws InstantiationException
+	 * Return a StringBuffer with colored tokens as specified from the
+	 * coloredTokens. There should be one-to-one correspondence with the actual
+	 * tokens.
 	 */
-	public static StringBuffer writeHTMLwithColors(
+	public StringBuffer writeHTMLwithColors(
 			final List<ColoredToken> coloredTokens, final File codeFile)
 			throws IOException, InstantiationException, IllegalAccessException {
 		final String code = FileUtils.readFileToString(codeFile);
 
 		final StringBuffer buf = new StringBuffer();
 
-		final ITokenizer jTokenizer = new JavaTokenizer(); // TODO: get from
-															// main
 		final SortedMap<Integer, FullToken> toks = jTokenizer
 				.fullTokenListWithPos(code.toCharArray());
 
 		int i = 0;
 		int prevPos = 0;
-		buf.append("<html><body style='font-family:monospace; background-color:rgb(200,200,255)'>");
+		buf.append("<html><body style='font-family:monospace; "
+				+ "background-color:rgb(" + documentBackgroundColor.getRed()
+				+ "," + documentBackgroundColor.getGreen() + ","
+				+ documentBackgroundColor.getBlue() + ")'>");
 		for (final Entry<Integer, FullToken> entry : toks.entrySet()) {
 			if (i == 0 || entry.getKey() == Integer.MAX_VALUE) {
 				i++;
@@ -93,7 +123,7 @@ public class JavaCodePrinter {
 
 			buf.append("<span style='background-color:rgb("
 					+ tok.bgColor.getRed() + "," + tok.bgColor.getGreen() + ","
-					+ tok.bgColor.getBlue() + "); color:rbg("
+					+ tok.bgColor.getBlue() + "); color:rgb("
 					+ tok.fontColor.getRed() + "," + tok.fontColor.getGreen()
 					+ "," + tok.fontColor.getBlue() + "); " + tok.extraStyle
 					+ "'>" + entry.getValue().token + "</span>");
@@ -103,9 +133,4 @@ public class JavaCodePrinter {
 		buf.append("</body></html>");
 		return buf;
 	}
-
-	public JavaCodePrinter() {
-		super();
-	}
-
 }
