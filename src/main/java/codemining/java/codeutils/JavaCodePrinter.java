43d840bdf62ca6a5c6c2e9fa2574531a62dec57a
Miltos Allamanis <m.allamanis@ed.ac.uk>
2014-03-24 23:02:28 +0000

    Minor renamings and refactoring

--- src/main/java/codemining/java/codeutils/JavaCodePrinter.java
+++ src/main/java/codemining/java/codeutils/JavaCodePrinter.java
@@ -9,6 +9,7 @@ import java.util.SortedMap;
 
 import org.apache.commons.io.FileUtils;
 
+import codemining.languagetools.ColoredToken;
 import codemining.languagetools.ITokenizer;
 import codemining.languagetools.ITokenizer.FullToken;
 
@@ -19,38 +20,6 @@ import codemining.languagetools.ITokenizer.FullToken;
  * 
  */
 public class JavaCodePrinter {
-
-	/**
-	 * Struct class to get a code colored token.
-	 * 
-	 */
-	public final static class ColoredToken {
-		public final Color fontColor;
-		public final Color bgColor;
-		public final String token;
-		public final String extraStyle;
-
-		/**
-		 * Construct with default bgColor white.
-		 * 
-		 * @param token
-		 * @param fontColor
-		 */
-		public ColoredToken(final String token, final Color fontColor) {
-			this.token = token;
-			this.fontColor = fontColor;
-			bgColor = Color.WHITE;
-			extraStyle = "";
-		}
-
-		public ColoredToken(final String token, final Color fontColor,
-				final Color bgColor, final String extraStyle) {
-			this.token = token;
-			this.fontColor = fontColor;
-			this.bgColor = bgColor;
-			this.extraStyle = extraStyle;
-		}
-	}
 
 	/**
 	 * The tokenizer used to tokenize code.
