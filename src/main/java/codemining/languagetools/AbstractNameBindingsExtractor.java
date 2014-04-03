a832883e228b97e613897f591966102b480ce3d5
Miltos Allamanis <m.allamanis@ed.ac.uk>
2014-04-03 10:46:49 +0100

    Some documentation comments.

--- src/main/java/codemining/languagetools/AbstractNameBindingsExtractor.java
+++ src/main/java/codemining/languagetools/AbstractNameBindingsExtractor.java
@@ -17,6 +17,15 @@ import com.google.common.collect.Multimap;
  * 
  */
 public abstract class AbstractNameBindingsExtractor {
+
+	/**
+	 * Return all the name bindings for file f
+	 * 
+	 * @param f
+	 * @return a multimap containing for each name all the relavant name
+	 *         bindings in the file.
+	 * @throws IOException
+	 */
 	public Multimap<String, TokenNameBinding> getBindingsForName(final File f)
 			throws IOException {
 		return getBindingsForName(getNameBindings(f));
@@ -31,6 +40,13 @@ public abstract class AbstractNameBindingsExtractor {
 		return toks;
 	}
 
+	/**
+	 * Return the name bindings given the code.
+	 * 
+	 * @param code
+	 * @return a multimap containing for each name all the relavant name
+	 *         bindings in the code snippet.
+	 */
 	public Multimap<String, TokenNameBinding> getBindingsForName(
 			final String code) {
 		return getBindingsForName(getNameBindings(code));
@@ -54,8 +70,21 @@ public abstract class AbstractNameBindingsExtractor {
 	 */
 	public abstract List<TokenNameBinding> getNameBindings(final String code);
 
+	/**
+	 * Return a ResolvedSourceCode instance for the given code.
+	 * 
+	 * @param f
+	 * @return
+	 * @throws IOException
+	 */
 	public abstract ResolvedSourceCode getResolvedSourceCode(final File f)
 			throws IOException;
 
+	/**
+	 * Return a ResolvedSourceCode instance for the given code.
+	 * 
+	 * @param code
+	 * @return
+	 */
 	public abstract ResolvedSourceCode getResolvedSourceCode(final String code);
 }
